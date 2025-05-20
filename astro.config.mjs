5f881086409cf77ec62437d19bb5bb0eacb09be1
Onwuemene Joshua <onwuemenejoshua@gmail.com>
2025-05-20 20:42:30 +0100

    first commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,37 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  integrations: [
+    starlight({
+      title: "Vue.js",
+      customCss: ["./src/styles/custom.css"],
+      logo: { src: "./src/assets/Image/Vue.js.svg" },
+      social: [
+        { icon: "github", label: "GitHub", href: "https://github.com/vuejs/" },
+      ],
+      sidebar: [
+        {
+          label: "Getting Started",
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: "Introduction", slug: "guides/introduction" },
+            { label: "Quick Start", slug: "guides/quickstart" },
+          ],
+        },
+        {
+          label: "Essentials",
+          items: [
+            {
+              label: "Creating An Application",
+              slug: "essentials/application",
+            },
+            { label: "Template Syntax", slug: "essentials/template" },
+          ],
+        },
+      ],
+    }),
+  ],
 });
